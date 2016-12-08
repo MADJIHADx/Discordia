0d4ff4fcd65ab3f289104717a3e776b4316f2c1c
SinisterRectus <sinister.rectus@gmail.com>
2016-12-07 19:34:20 -0500

    Removed waveform generators from library

--- discordia/voice/VoiceClient.lua
+++ discordia/voice/VoiceClient.lua
@@ -1,13 +1,9 @@
 local ClientBase = require('../utils/ClientBase')
 local VoiceConnection = require('./VoiceConnection')
 local constants = require('./constants')
-local waveforms = require('./waveforms')
 
 local CHANNELS = constants.CHANNELS
 local SAMPLE_RATE = constants.SAMPLE_RATE
-local SAMPLE_PERIOD = constants.SAMPLE_PERIOD
-local MAX_INT16 = constants.MAX_INT16
-local TAU = constants.TAU
 
 local defaultOptions = {
 	dateTime = '%c',
@@ -94,28 +90,6 @@ function VoiceClient:stopStreams()
 		if connection._stream then
 			connection._stream:stop()
 		end
-	end
-end
-
-function VoiceClient:createMonoToneGenerator(name, freq, amplitude) -- luacheck: ignore self
-	local h = TAU * freq / SAMPLE_RATE
-	local a = (amplitude or 1) * MAX_INT16
-	return waveforms[name](h, a)
-end
-
-function VoiceClient:createPolyToneGenerator(args) -- luacheck: ignore self
-	local n = 1 / #args
-	for i, v in ipairs(args) do
-		local h = TAU * v[2] * SAMPLE_PERIOD
-		local a = (v[3] or n) * MAX_INT16
-		args[i] = waveforms[v[1]](h, a)
-	end
-	return function()
-		local s = 0
-		for _, waveform in ipairs(args) do
-			s = s + waveform()
-		end
-		return s, s
 	end
 end
 
